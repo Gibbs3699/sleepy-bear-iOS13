4b8394f984a1445abedba5d243146811f0f7ced6
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-04 22:50:54 +0700

    Adding LottieView to show sleepy bear

--- Sleepy bear/ContentView.swift
+++ Sleepy bear/ContentView.swift
@@ -11,14 +11,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.7490196078, green: 0.2117647059, blue: 0.04705882353, alpha: 1)), Color(#colorLiteral(red: 0.3058823529, green: 0.2039215686, blue: 0.1803921569, alpha: 1))]), startPoint: .top, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.7490196078, green: 0.2117647059, blue: 0.04705882353, alpha: 0.6980550399)), Color(#colorLiteral(red: 0.3058823529, green: 0.2039215686, blue: 0.1803921569, alpha: 0.5052635749))]), startPoint: .top, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
+                LottieView(name: "42828-sleeping-bear", loopMode: .loop)
+                    .frame(width: 250, height: 250)
+
+                Spacer()
+                
                 Text("Sleepy Bear!")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
                     .foregroundColor(.white)
                 
             }
+            .frame(width: 300, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
             
     }
